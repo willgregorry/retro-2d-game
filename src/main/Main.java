49ce6ba436113a946547049e76073555671a3113
wilzzzzzzzz <itswgxx@gmail.com>
2025-05-19 02:02:39 +0700

    feat: add movement with keylistener

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,9 +7,16 @@ public class Main {
         JFrame window = new JFrame("Retro 2D Game");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.add(new GamePanel());
-        window.setLocationRelativeTo(null);
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
+
     }
 }
